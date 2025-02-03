16e9bff454e68e2fa797776bd36f582b2cd718b6
quochuy242 <truongquochuy.240203@gmail.com>
2025-02-03 12:24:30 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 import sys
 
+def check_builtin_command(cmd: str):
+    builtin_cmds = ["echo", 'exit', 'type']
+    if cmd in builtin_cmds:
+        print(f'{cmd} is a shell builtin')
+    else: 
+        print(f"{cmd}: not found")
 
 def main():
     while True:
@@ -8,6 +14,8 @@ def main():
             return 0
         elif cmd[:4] == 'echo':
             print(cmd[5:])
+        elif cmd[:4] == 'type':
+            check_builtin_command(cmd[5:])
         else:
             print(f"{cmd}: command not found")
   
