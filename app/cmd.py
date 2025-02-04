a65d550c055e1665f5566f9caa7bbe1f599de077
quochuy242 <truongquochuy.240203@gmail.com>
2025-02-04 05:41:29 +0000

    codecrafters submit [skip ci]

---
+++ app/cmd.py
@@ -0,0 +1,102 @@
+import os
+import subprocess
+import sys
+from typing import List, Optional, Tuple
+
+from .helper import check_builtin_command, check_exists_dir, find_exec_path
+
+
+def cmd_echo(cmd: str) -> None:
+    try:
+        # Check if the command is echo
+        check = cmd.split(" ")[0] == "echo"
+        if check:
+            sys.stdout.write(f"{cmd.replace('echo ', '')}\n")
+    except Exception as e:
+        raise e
+    return
+
+
+def cmd_exit(cmd: str) -> None:
+    try:
+        # Check if the command is exit
+        check = cmd.split(" ")[0] == "exit"
+        if check:
+            exit(0)
+    except Exception as e:
+        raise e
+
+
+def cmd_type(cmd: str) -> None:
+    try:
+        # Check if the command is type
+        check = cmd.split(" ")[0] == "type"
+        if not check:
+            raise Exception("Not a type command\n")
+
+        # Check if the command is valid and print the result
+        content = cmd.replace("type ", "")
+        builtin_check = check_builtin_command(
+            content
+        )  # Check if the command is a shell builtin
+        exec_path = find_exec_path(content)  # Check if the command is an executable
+
+        # Print the result
+        if builtin_check:
+            sys.stdout.write(f"{content} is a shell builtin\n")
+        elif exec_path is not None:
+            sys.stdout.write(f"{content} is {exec_path}\n")
+        else:
+            sys.stdout.write(f"{content}: not found\n")
+    except Exception as e:
+        raise e
+    return
+
+
+def run_program(program_name: str, args: List[str]):
+    try:
+        # Run the program
+        subprocess.run([program_name, *args])
+    except Exception as e:
+        raise e
+
+
+def pwd() -> str:
+    try:
+        curr_dir = os.getcwd()
+        sys.stdout.write(f"{curr_dir}\n")
+    except Exception as e:
+        raise e
+    return curr_dir
+
+
+def cd(directory: str):
+    try:
+        # If the directory is absolute
+        if directory.startswith("/"):
+            # Check the new directory exists
+            if not check_exists_dir(directory):
+                return
+            os.chdir(directory)
+            return
+        else:  # If the directory is relative
+            # Get the current directory
+            curr_dir = os.getcwd()
+            dirs = directory.split("/")
+
+            for element in dirs:
+                if element == "..":
+                    curr_dir = os.path.dirname(curr_dir)
+                elif element == ".":
+                    continue
+                else:
+                    curr_dir = os.path.join(curr_dir, element)
+
+            # Check the new directory exists
+            if not check_exists_dir(curr_dir):
+                return
+
+            # Change the directory
+            os.chdir(curr_dir)
+    except Exception as e:
+        raise e
--- app/main.py
+++ app/main.py
@@ -3,95 +3,8 @@ import subprocess
 import sys
 from typing import List, Optional, Tuple
 
-from .helpers import check_builtin_command, check_exists_dir, find_exec_path
-
-
-def handle_echo(cmd: str) -> None:
-    try:
-        # Check if the command is echo
-        check = cmd.split(" ")[0] == "echo"
-        if check:
-            sys.stdout.write(f"{cmd.replace('echo ', '')}\n")
-    except Exception as e:
-        raise e
-    return
-
-
-def handle_exit(cmd: str) -> None:
-    try:
-        # Check if the command is exit
-        check = cmd.split(" ")[0] == "exit"
-        if check:
-            exit(0)
-    except Exception as e:
-        raise e
-
-
-def handle_type(cmd: str) -> None:
-    try:
-        # Check if the command is type
-        check = cmd.split(" ")[0] == "type"
-        if not check:
-            raise Exception("Not a type command\n")
-
-        # Check if the command is valid and print the result
-        content = cmd.replace("type ", "")
-        builtin_check = check_builtin_command(
-            content
-        )  # Check if the command is a shell builtin
-        exec_path = find_exec_path(content)  # Check if the command is an executable
-
-        # Print the result
-        if builtin_check:
-            sys.stdout.write(f"{content} is a shell builtin\n")
-        elif exec_path is not None:
-            sys.stdout.write(f"{content} is {exec_path}\n")
-        else:
-            sys.stdout.write(f"{content}: not found\n")
-    except Exception as e:
-        raise e
-    return
-
-
-def run_program(program_name: str, args: List[str]):
-    try:
-        # Run the program
-        subprocess.run([program_name, *args])
-    except Exception as e:
-        raise e
-
-
-def pwd() -> str:
-    try:
-        curr_dir = os.getcwd()
-        sys.stdout.write(f"{curr_dir}\n")
-    except Exception as e:
-        raise e
-    return curr_dir
-
-
-def cd(directory: str):
-    try:
-        # If the directory is absolute
-        if directory.startswith("/"):
-            # Check the new directory exists
-            if not check_exists_dir(directory):
-                return
-            os.chdir(directory)
-            return
-        else:  # If the directory is relative
-            # Get the current directory
-            curr_dir = pwd()
-            abs_dir = os.path.join(curr_dir, directory)
-
-            # Check the new directory exists
-            if not check_exists_dir(abs_dir):
-                return
-
-            # Change the directory
-            os.chdir(abs_dir)
-    except Exception as e:
-        raise e
+from .helper import check_builtin_command, check_exists_dir, find_exec_path
+from .cmd import cmd_echo, cmd_exit, cmd_type, pwd, cd, run_program
 
 
 def main():
@@ -102,13 +15,13 @@ def main():
         # Check if the command is a shell builtin
         if check_builtin_command(command):
             if command == "echo":
-                handle_echo(user_input)
+                cmd_echo(user_input)
                 continue
             if command == "exit":
-                handle_exit(user_input)
+                cmd_exit(user_input)
                 continue
             if command == "type":
-                handle_type(user_input)
+                cmd_type(user_input)
                 continue
             if command == "pwd":
                 pwd()
